69937da987fad1701e70cbd30919af4f88d64445
HSellappan <hasellan@gmail.com>
2025-10-24 15:55:11 -0500

    Initial Commit

---
+++ MovingWallpaper/MovingWallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovingWallpaperApp.swift
+//  MovingWallpaper
+//
+//  Created by Harold S on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MovingWallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
